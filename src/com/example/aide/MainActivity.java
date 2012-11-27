feb988fe2639241312c3d24281133ca4b0c18633
Sebastien Duriatti <sebduriatti1@gmail.com>
2012-11-27 19:02:46 +0100

    Start
    Début de l'appli "aide" (attention, je pense supprimer /bin)

---
+++ src/com/example/aide/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.aide;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private TextView coucou = null;
+    		
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		coucou = new TextView(this);
+		coucou.setText("Bonjour les encoulées");
+		setContentView(coucou);
+	}
+
+}
